bf15b6152777811bfbf11e6a259c35bf8db2accc
lemcur <loernittcc@gmail.com>
2018-07-06 11:33:42 +0200

    root index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :quotes
+  root "quotes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
