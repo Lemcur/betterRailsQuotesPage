df3304ff9c07674ed6f4572d32ffc5942a21304b
lemcur <loernittcc@gmail.com>
2018-08-18 14:54:27 +0200

    added a bit of styling for mobile and deleted some useless js

--- app/assets/javascripts/new_form.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-	var isValidForm= function(){
-		if(document.getElementById("quote_content").value.trim()==="" || document.getElementById("quote_author").value.trim() === ""){
-			return false;
-		}
-		return true;
-	}
-	document.getElementsByTagName("form")[0].onsubmit = isValidForm;
-}
